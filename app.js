222a844f9f067b9f03bdb3518600124afd854cd0
Alice Cutler <acutler0451@gmail.com>
2022-02-02 21:47:02 -0500

    not sure how i want to start

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+const http = require('http');
+const config = require('./config.js');
+const uri = config.redirect_uri;
+const apiKey = config.API_key;
+// const date = new Date(document.lastModified);
+// document.getElementById('modified').innerHTML = date;
+
+const app = express();
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+// app.post('/', function (req, res) {});
+
+app.listen(8888, function () {
+	console.log('Listening on 8888');
+});
